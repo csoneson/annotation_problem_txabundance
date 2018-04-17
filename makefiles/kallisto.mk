ad83f917ee5e8b104ac03fd8b944112ed3b648fb
Charlotte Soneson <charlottesoneson@gmail.com>
2018-04-17 22:59:53 +0200

    kallisto and StringTie with strand-specificity.

--- makefiles/kallisto.mk
+++ makefiles/kallisto.mk
@@ -16,9 +16,9 @@ $(foreach F,$(fastqfiles),$(eval $(call kallistoindexrule,reference/kallisto/$(n
 ## ==================================================================================== ##
 ## Run kallisto
 define kallistorule
-$(4)/$(2)/abundance.tsv: $(3)# $(1)_R1.fastq.gz $(1)_R2.fastq.gz
+$(4)/$(2)/abundance.tsv: $(3) $(1)_R1.fastq.gz $(1)_R2.fastq.gz
 	mkdir -p $$(@D)
-	$(kallisto) quant -i $(3) -o $$(@D) --bias -t $(nthreads) $(1)_R1.fastq.gz $(1)_R2.fastq.gz
+	$(kallisto) quant -i $(3) -o $$(@D) --bias -t $(nthreads) $(5) $(1)_R1.fastq.gz $(1)_R2.fastq.gz
 endef
-$(foreach F,$(fastqfiles),$(eval $(call kallistorule,$(F),$(notdir $(F)),$(kallistocdnancrnaindex),kallisto/cDNAncRNA)))
-$(foreach F,$(fastqfiles),$(eval $(call kallistorule,$(F),$(notdir $(F)),reference/kallisto/$(notdir $(F))_stringtie_tx_kidx_v0.44.0,kallisto_stringtie_tx)))
+$(foreach F,$(fastqfiles),$(eval $(call kallistorule,$(F),$(notdir $(F)),$(kallistocdnancrnaindex),kallisto/cDNAncRNA,--rf-stranded)))
+$(foreach F,$(fastqfiles),$(eval $(call kallistorule,$(F),$(notdir $(F)),reference/kallisto/$(notdir $(F))_stringtie_tx_kidx_v0.44.0,kallisto_stringtie_tx,--rf-stranded)))

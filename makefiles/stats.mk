d306f03edfa067207402ad8158c60909ae7eb51d
Charlotte Soneson <charlottesoneson@gmail.com>
2018-06-29 16:34:53 +0200

    Add quantmethods to get_highscore_genes call

--- makefiles/stats.mk
+++ makefiles/stats.mk
@@ -12,7 +12,7 @@ define highscorerule
 stats/genes_with_high_score_$(1)$(2).txt: output/$(1)$(2)_combined_coverages_with_scores.rds \
 Rscripts/get_highscore_genes.R
 	mkdir -p $$(@D)
-	$(R) "--args scorerds='$$<' uniqjuncreadsthreshold=$(uniqjuncreadsthreshold) outtxt='$$@'" Rscripts/get_highscore_genes.R Rout/get_highscore_genes_$(1)$(2).Rout
+	$(R) "--args scorerds='$$<' quantmethods='hera,kallisto,RSEM,Salmon,SalmonSTAR,SalmonCDS,SalmonKeepDup,StringTie' uniqjuncreadsthreshold=$(uniqjuncreadsthreshold) outtxt='$$@'" Rscripts/get_highscore_genes.R Rout/get_highscore_genes_$(1)$(2).Rout
 endef
 $(foreach F,$(fastqfiles),$(eval $(call highscorerule,$(notdir $(F)),)))
 $(foreach F,$(fastqfiles),$(eval $(call highscorerule,$(notdir $(F)),_stringtie_tx)))
